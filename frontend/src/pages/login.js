84688747073af864326ebb613334feb7f73bd67f
haroldhang <haroldadjahi@gmai.com>
2023-12-20 16:34:01 +0100

    adding login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -21,6 +21,7 @@ const Login = () => {
                         <button className="btn-primary btn-primary--stand">
                             Sign in
                         </button>
+                        
                     </div>
                 </form>
 
